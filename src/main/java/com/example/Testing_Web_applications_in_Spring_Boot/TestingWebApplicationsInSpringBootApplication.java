4416a578888b0dd3f0f344e3d5c380860ea66dfc
DiplineRU <artgul52@gmail.com>
2025-02-20 16:29:42 +0300

    initial commit

---
+++ src/main/java/com/example/Testing_Web_applications_in_Spring_Boot/TestingWebApplicationsInSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.example.Testing_Web_applications_in_Spring_Boot;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class TestingWebApplicationsInSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingWebApplicationsInSpringBootApplication.class, args);
+	}
+
+}
